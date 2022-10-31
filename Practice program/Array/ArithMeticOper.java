6e4cd24f44b70142d4e67d15a21591437305f421
hemant patel <hemant00097@gmail.com>
2022-10-31 12:20:58 +0530

    prog

---
+++ Practice program/Array/ArithMeticOper.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArithMeticOper {
+    public static void main(String[] args) 
+    {
+        
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+
+        System.out.println("sum is : " +(a+b));
+        System.out.println("difference is : "+(a-b));
+        System.out.println("product is :"+ (a*b));
+        System.out.println("Qutient is : "+(a/b));
+        System.out.println("Rimender is : "+(a%b));
+    }
+}
---
+++ programming class/chapter 3 pattern/Q37.java
@@ -0,0 +1,36 @@
+/*
+Print the following pattern
+* * * * * * *
+* *   *   * *
+*   * * *   *
+* * * * * * * 
+*   * * *   *
+* *   *   * *
+* * * * * * *                               */
+
+import java.util.Scanner;
+public class Q37 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number ");
+        int n = sc.nextInt();
+        sc.close();
+
+        for(int i=1; i<=n; i++) 
+        {
+            for(int j=1; j<=n; j++) 
+            {
+                if(j==1 || j==n || i==1 || i==n || i==n/2+1 || j==n/2+1 ||  i==j || i+j == n+1) 
+                {
+                    System.out.print("* ");
+                }
+                else 
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
